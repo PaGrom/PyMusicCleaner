7299f79c77db7cdd06b48a81e2d1dd3f0cf375ad
PaGrom <pagrom23@gmail.com>
2012-09-30 23:37:11 +0400

    add pylast

--- PyMusicCleaner.py
+++ PyMusicCleaner.py
@@ -1,10 +1,23 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*
 
-"""PyMusicCleaner"""
+""" PyMusicCleaner takes info from last.fm,
+	edits music tags and renames files and directories. """
 
 import sys, os
 import eyed3
+import pylast
+
+# You have to have your own unique two values for API_KEY and API_SECRET
+# Obtain yours from http://www.last.fm/api/account for Last.fm
+API_KEY = "api_key"
+API_SECRET = "api_secret"
+
+username = "name"
+password_hash = pylast.md5("password")
+
+network = pylast.LastFMNetwork(api_key = API_KEY, api_secret = 
+    API_SECRET, username = username, password_hash = password_hash)
 
 def lister(root):
 	"""Выводит список файлов в дереве каталогов с помощью os.walk"""
@@ -16,7 +29,11 @@ def lister(root):
 				# print(path)
 
 				audiofile = eyed3.load(path)
-				print(audiofile.tag.artist)
+				# print(audiofile.tag.artist)
+
+				artist = network.get_artist(audiofile.tag.artist)
+
+				print(artist.get_name())
 
 if __name__ == '__main__':
 	lister(sys.argv[1])
